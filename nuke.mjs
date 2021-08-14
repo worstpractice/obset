5a60c14bd72850543fe7614f5d735a1da30cc969
worstpractice <54796463+worstpractice@users.noreply.github.com>
2021-08-14 17:02:23 +0200

    improve build cleanup script

--- nuke.mjs
+++ nuke.mjs
@@ -2,12 +2,16 @@ import { promises } from 'fs';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const { rm } = promises;
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const getProjectRoot = () => {
+  // @ts-ignore `import.meta` complaints, not a real concern
+  return globalThis.__dirname || dirname(fileURLToPath(import.meta.url));
+};
 
 const nuke = async () => {
-  await rm(`${__dirname}/dist`, { recursive: true });
+  // @ts-ignore we know, node 12. we know.
+  const rm = promises.rm || promises.rmdir;
+
+  await rm(`${getProjectRoot()}/dist`, { recursive: true });
 };
 
 const noOp = () => {};
