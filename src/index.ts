55dea00a866b9cddaddbdef711e4e681df931e5d
worstpractice <54796463+worstpractice@users.noreply.github.com>
2021-08-16 14:48:35 +0200

    2.0 🎉

--- src/index.ts
+++ src/index.ts
@@ -26,18 +26,16 @@ export interface SetEventTarget<T> {
 // prettier-ignore
 export type SetOperation =
   | "add"
+  | "clear"
   | "delete"
+  | "empty"
 ;
 
-export type SetOperationEvent<T> = Omit<SetEvent<T>, 'operation'>;
-
-export type SetOperationEventListener<T> = (event: SetOperationEvent<T>) => void;
-
-export type SetOperationEventListeners<T> = {
-  readonly [key in SetOperation]: Set<SetOperationEventListener<T>>;
+export type Listeners<T> = {
+  readonly [key in SetOperation]: Set<SetEventListener<T>>;
 };
 
-export type SetOperationListeners<T> = {
+export type MaybeListeners<T> = {
   [key in SetOperation]?: Set<SetEventListener<T>>;
 };
 
@@ -52,19 +50,32 @@ const DEFAULT_OPTIONS: ObSetOptions = {
   freeUnusedResources: true,
 } as const;
 
+type WarnIfMissingKey = {
+  readonly [key in SetOperation]: undefined;
+};
+
+const warnIfMissingKey: WarnIfMissingKey = {
+  add: undefined,
+  clear: undefined,
+  delete: undefined,
+  empty: undefined,
+} as const;
+
+const SET_OPERATIONS = Object.keys(warnIfMissingKey);
+
 export class ObSet<T> extends Set<T> implements SetEventTarget<T> {
-  private readonly onAnyHandlers: SetOperationEventListeners<T> = {
-    add: new Set<SetOperationEventListener<T>>(),
-    delete: new Set<SetOperationEventListener<T>>(),
+  private readonly operationListeners: Listeners<T> = {
+    add: new Set<SetEventListener<T>>(),
+    clear: new Set<SetEventListener<T>>(),
+    delete: new Set<SetEventListener<T>>(),
+    empty: new Set<SetEventListener<T>>(),
   } as const;
 
-  private readonly onEmptyHandlers = new Set<SetOperationEventListener<T>>();
-
-  private readonly onValueHandlers: Map<T, SetOperationListeners<T>> = new Map<T, SetOperationListeners<T>>();
+  private readonly valueListeners: Map<T, MaybeListeners<T>> = new Map<T, MaybeListeners<T>>();
 
   private readonly options: ObSetOptions;
 
-  private readonly toBeRanOnlyOnce: SetEventListener<T>[] = [];
+  private readonly oneTimeListeners: SetEventListener<T>[] = [];
 
   constructor(initialValues?: Iterable<T>, options?: ObSetOptions) {
     super();
@@ -73,7 +84,7 @@ export class ObSet<T> extends Set<T> implements SetEventTarget<T> {
     if (!initialValues) return this;
 
     for (const value of initialValues) {
-      this.add(value);
+      super.add(value);
     }
   }
 
@@ -92,17 +103,8 @@ export class ObSet<T> extends Set<T> implements SetEventTarget<T> {
     return this;
   }
 
-  addEventListener(this: this, operation: SetOperation, value: T, listener: SetEventListener<T>, options?: SetEventListenerOptions): this {
-    const operationListeners = this.onValueHandlers.get(value) ?? this.initOperationListenersFor(value);
-
-    const eventListeners = operationListeners[operation] ?? this.initEventListenersFor(operation, operationListeners);
-
-    eventListeners.add(listener);
-
-    if (options?.once) this.toBeRanOnlyOnce.push(listener);
-
-    return this;
-  }
+  /** NOTE: syntactic sugar. */
+  addEventListener = this.onValue;
 
   override clear(this: this): this {
     for (const value of this) {
@@ -116,25 +118,20 @@ export class ObSet<T> extends Set<T> implements SetEventTarget<T> {
     const clone: ObSet<T> = new ObSet<T>(this, this.options);
 
     // Copy anyHandlers
-    for (const operation of Object.keys(this.onAnyHandlers)) {
-      for (const handler of this.onAnyHandlers[operation]) {
-        clone.onAnyHandlers[operation].add(handler);
+    for (const operation of SET_OPERATIONS) {
+      for (const handler of this.operationListeners[operation]) {
+        clone.operationListeners[operation].add(handler);
       }
     }
 
-    // Copy emptyHandlers
-    for (const handler of this.onEmptyHandlers) {
-      clone.onEmptyHandlers.add(handler);
-    }
-
     // Copy valueHandlers
-    for (const [value, handler] of this.onValueHandlers) {
-      clone.onValueHandlers.set(value, handler);
+    for (const [value, handler] of this.valueListeners) {
+      clone.valueListeners.set(value, handler);
     }
 
     // Copy one-off handlers
-    for (const handler of this.toBeRanOnlyOnce) {
-      clone.toBeRanOnlyOnce.push(handler);
+    for (const handler of this.oneTimeListeners) {
+      clone.oneTimeListeners.push(handler);
     }
 
     return clone;
@@ -154,52 +151,49 @@ export class ObSet<T> extends Set<T> implements SetEventTarget<T> {
     this.dispatchEvent(event);
 
     if (!this.size) {
-      const operationEvent: SetOperationEvent<T> = {
+      const setEvent: SetEvent<T> = {
+        operation: 'empty',
         value: event.value,
       } as const;
 
-      for (const listener of this.onEmptyHandlers) {
-        listener.call(this, operationEvent);
+      for (const listener of this.operationListeners.empty) {
+        listener.call(this, setEvent);
       }
     }
 
     return this;
   }
 
-  private deleteOneTimeListener(this: this, listener: SetEventListener<T> | SetOperationEventListener<T>): void {
-    const listenerIndex = this.toBeRanOnlyOnce.indexOf(listener);
+  private deleteOneTimeListener(this: this, listener: SetEventListener<T> | SetEventListener<T>): void {
+    const listenerIndex = this.oneTimeListeners.indexOf(listener);
 
     if (listenerIndex === -1) return;
 
-    swapPop(this.toBeRanOnlyOnce, listenerIndex);
+    swapPop(this.oneTimeListeners, listenerIndex);
   }
 
   private dispatchEvent(this: this, event: SetEvent<T>): this {
     const { operation, value } = event;
 
-    const anyListeners = this.onAnyHandlers[operation];
-
-    const operationEvent: SetOperationEvent<T> = {
-      value,
-    } as const;
+    const anyListeners = this.operationListeners[operation];
 
     for (const listener of anyListeners) {
-      listener.call(this, operationEvent);
+      listener.call(this, event);
 
-      if (!this.toBeRanOnlyOnce.includes(listener)) continue;
+      if (!this.oneTimeListeners.includes(listener)) continue;
 
       anyListeners.delete(listener);
       this.deleteOneTimeListener(listener);
     }
 
-    const eventListeners = this.onValueHandlers.get(value)?.[operation];
+    const eventListeners = this.valueListeners.get(value)?.[operation];
 
     if (!eventListeners) return this;
 
     for (const listener of eventListeners) {
       listener.call(this, event);
 
-      if (!this.toBeRanOnlyOnce.includes(listener)) continue;
+      if (!this.oneTimeListeners.includes(listener)) continue;
 
       eventListeners.delete(listener);
       this.deleteOneTimeListener(listener);
@@ -208,7 +202,7 @@ export class ObSet<T> extends Set<T> implements SetEventTarget<T> {
     return this;
   }
 
-  private findOperationsWithoutListenersIn(this: this, operationListeners: SetOperationListeners<T>): readonly SetOperation[] {
+  private findOperationsWithoutListenersIn(this: this, operationListeners: MaybeListeners<T>): readonly SetOperation[] {
     const operationSetPairs = Object.entries(operationListeners);
 
     const operationsWithoutListeners: SetOperation[] = [];
@@ -225,7 +219,7 @@ export class ObSet<T> extends Set<T> implements SetEventTarget<T> {
   /** NOTE: keeps memory usage as low as possible, at the cost of some extra cleanup work.
    *
    * See: https://en.wikipedia.org/wiki/Space%E2%80%93time_tradeoff */
-  private freeUnusedResourcesIn(this: this, operationListeners: SetOperationListeners<T>, value: T): void {
+  private freeUnusedResourcesIn(this: this, operationListeners: MaybeListeners<T>, value: T): void {
     const withoutListeners: readonly SetOperation[] = this.findOperationsWithoutListenersIn(operationListeners);
 
     // Free any sets without listeners
@@ -236,10 +230,10 @@ export class ObSet<T> extends Set<T> implements SetEventTarget<T> {
     if (Object.keys(operationListeners).length) return;
 
     // Free any values without sets
-    this.onValueHandlers.delete(value);
+    this.valueListeners.delete(value);
   }
 
-  private initEventListenersFor(this: this, operation: SetOperation, operationListeners: SetOperationListeners<T>): Set<SetEventListener<T>> {
+  private initEventListenersFor(this: this, operation: SetOperation, operationListeners: MaybeListeners<T>): Set<SetEventListener<T>> {
     const eventListeners = new Set<SetEventListener<T>>();
 
     operationListeners[operation] = eventListeners;
@@ -247,43 +241,103 @@ export class ObSet<T> extends Set<T> implements SetEventTarget<T> {
     return eventListeners;
   }
 
-  private initOperationListenersFor(this: this, value: T): SetOperationListeners<T> {
-    const operationListeners: SetOperationListeners<T> = {} as const;
+  private initOperationListenersFor(this: this, value: T): MaybeListeners<T> {
+    const operationListeners: MaybeListeners<T> = {} as const;
 
-    this.onValueHandlers.set(value, operationListeners);
+    this.valueListeners.set(value, operationListeners);
 
     return operationListeners;
   }
 
-  /** NOTE: syntactic sugar for `addEventListener`. */
-  readonly on = this.addEventListener;
+  on(this: this, operation: SetOperation, listener: SetEventListener<T>, options?: SetEventListenerOptions): this;
+  on(this: this, operation: SetOperation, value: T, listener: SetEventListener<T>, options?: SetEventListenerOptions): this;
+  on(this: this, ...args: readonly [operation: SetOperation, valueOrListener: T | SetEventListener<T>, listenerOrOptions?: SetEventListener<T> | SetEventListenerOptions, maybeOptions?: SetEventListenerOptions]): this {
+    const [operation, valueOrListener, listenerOrOptions, maybeOptions] = args;
 
-  onAny(this: this, operation: SetOperation, listener: SetOperationEventListener<T>): this {
-    this.onAnyHandlers[operation].add(listener);
+    switch (args.length) {
+      case 2: {
+        const listener = valueOrListener as SetEventListener<T>;
+        const options = listenerOrOptions as SetEventListenerOptions | undefined;
+
+        return this.onOperation(operation, listener, options);
+      }
+
+      case 3: {
+        if (typeof listenerOrOptions === 'function') {
+          const value = valueOrListener as T;
+          const listener = listenerOrOptions;
+
+          return this.onValue(operation, value, listener, maybeOptions);
+        }
+
+        const listener = valueOrListener as SetEventListener<T>;
+        const options = listenerOrOptions as SetEventListenerOptions | undefined;
+
+        return this.onOperation(operation, listener, options);
+      }
+
+      case 4: {
+        const value = valueOrListener as T;
+        const listener = listenerOrOptions as SetEventListener<T>;
+
+        return this.onValue(operation, value, listener, maybeOptions);
+      }
+
+      default: {
+        return this.unreachable(args.length);
+      }
+    }
+  }
+
+  private onOperation(this: this, operation: SetOperation, listener: SetEventListener<T>, options?: SetEventListenerOptions): this {
+    this.operationListeners[operation].add(listener);
+
+    if (options?.once) this.oneTimeListeners.push(listener);
 
     return this;
   }
 
-  once(this: this, operation: SetOperation, value: T, listener: SetEventListener<T>): this {
-    return this.on(operation, value, listener, ONCE);
-  }
+  private onValue(this: this, operation: SetOperation, value: T, listener: SetEventListener<T>, options?: SetEventListenerOptions): this {
+    const operationListeners = this.valueListeners.get(value) ?? this.initOperationListenersFor(value);
 
-  onceAny(this: this, operation: SetOperation, listener: SetOperationEventListener<T>): this {
-    this.onAnyHandlers[operation].add(listener);
+    const eventListeners = operationListeners[operation] ?? this.initEventListenersFor(operation, operationListeners);
 
-    this.toBeRanOnlyOnce.push(listener);
+    eventListeners.add(listener);
 
-    return this;
-  }
-
-  onEmpty(this: this, listener: SetOperationEventListener<T>): this {
-    this.onEmptyHandlers.add(listener);
+    if (options?.once) this.oneTimeListeners.push(listener);
 
     return this;
+  }
+
+  once(this: this, operation: SetOperation, listener: SetEventListener<T>, options?: SetEventListenerOptions): this;
+  once(this: this, operation: SetOperation, value: T, listener: SetEventListener<T>, options?: SetEventListenerOptions): this;
+  once(this: this, ...args: readonly [operation: SetOperation, valueOrListener: T | SetEventListener<T>, listenerOrOptions?: SetEventListener<T> | SetEventListenerOptions, maybeOptions?: SetEventListenerOptions]): this {
+    const [operation, valueOrListener, listenerOrOptions = {}, maybeOptions = {}] = args;
+
+    if (typeof listenerOrOptions === 'function') {
+      const originalOptions = maybeOptions;
+
+      return this.on(
+        operation,
+        // @ts-expect-error this only errors due to visibility rules for overloads
+        valueOrListener,
+        listenerOrOptions,
+        { ...originalOptions, ...ONCE } as const,
+      );
+    }
+
+    const originalOptions = listenerOrOptions;
+
+    return this.on(
+      operation,
+      // @ts-expect-error this only errors due to visibility rules for overloads
+      valueOrListener,
+      { ...originalOptions, ...ONCE } as const,
+    );
   }
 
   removeEventListener(this: this, operation: SetOperation, value: T, listener: SetEventListener<T>): this {
-    const operationListeners = this.onValueHandlers.get(value);
+    const operationListeners = this.valueListeners.get(value);
 
     if (!operationListeners) return this;
 
@@ -297,5 +351,9 @@ export class ObSet<T> extends Set<T> implements SetEventTarget<T> {
     if (this.options.freeUnusedResources) this.freeUnusedResourcesIn(operationListeners, value);
 
     return this;
+  }
+
+  private unreachable(length: never): never {
+    throw new RangeError(length);
   }
 }
